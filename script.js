eb69dd0d3a7c64d37c70b7a82f57d6f97576c9bc
Chaion Saha <sahachaion2000@gmail.com>
2022-12-13 21:00:33 +0600

    bug fixed

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 'use strict';
 
-const text = document.querySelector('.text');
+const text = document.querySelector('#circular-text');
 const splittedText = text.innerText.toUpperCase().split('');
-let computedStyle = window.getComputedStyle(
-	document.querySelector('.main-div')
-);
 
-const radius = parseInt(computedStyle.getPropertyValue('height')) / 2,
-	diameter = radius * 2;
+let computedStyle = window.getComputedStyle(text.parentElement);
 
-text.style.width = `${diameter}px`;
-text.style.height = `${diameter}px`;
+const circularText = () => {
+	let radius = parseInt(computedStyle.getPropertyValue('height')) / 2,
+		diameter = radius * 2;
+	text.style.width = `${diameter}px`;
+	text.style.height = `${diameter}px`;
 
-const angleToRadian = (angle) => {
-	return angle * (Math.PI / 180);
+	const angleToRadian = (angle) => {
+		return angle * (Math.PI / 180);
+	};
+
+	let angle = -90,
+		deltaAngle = 360 / splittedText.length;
+
+	text.innerText = null;
+	splittedText.forEach((letter, index) => {
+		const newSpan = document.createElement('span');
+		newSpan.innerText = letter;
+
+		let xPos = radius * Math.cos(angleToRadian(angle)) + radius;
+		let yPos = radius * Math.sin(angleToRadian(angle)) + radius;
+
+		angle += deltaAngle;
+		const transform = `translate(${xPos}px, ${yPos}px)`;
+		const rotate = `rotate(${index * deltaAngle}deg)`;
+		newSpan.style.transform = `${transform} ${rotate}`;
+		text.appendChild(newSpan);
+	});
 };
 
-let angle = -90,
-	deltaAngle = 360 / splittedText.length;
-
-text.innerText = null;
-splittedText.forEach((letter, index) => {
-	const newSpan = document.createElement('span');
-	newSpan.innerText = letter;
-
-	let xPos = radius * Math.cos(angleToRadian(angle)) + radius;
-	let yPos = radius * Math.sin(angleToRadian(angle)) + radius;
-
-	angle += deltaAngle;
-	const transform = `translate(${xPos}px, ${yPos}px)`;
-	const rotate = `rotate(${index * deltaAngle}deg)`;
-	newSpan.style.transform = `${transform} ${rotate}`;
-	text.appendChild(newSpan);
+circularText();
+window.addEventListener('resize', () => {
+	circularText();
 });
